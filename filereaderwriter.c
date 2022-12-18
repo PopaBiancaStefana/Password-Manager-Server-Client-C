6426e584a06843813a8ec7cc8394e33f04fc5d9c
POPA Bianca-Stefana <popabianca945@yahoo.com>
2022-12-18 17:47:59 -0500

    define functions for each command

--- filereaderwriter.c
+++ filereaderwriter.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include<stdbool.h>  
+#include <stdbool.h>
 
 struct password
 {
@@ -11,7 +11,6 @@ struct password
     char passwrd[30];
     char url[30];
     char notes[50];
-    bool deleted;
 };
 
 typedef struct person
@@ -21,15 +20,10 @@ typedef struct person
     char masterPassword[20];
     char categories[20][30];
     struct password passwords[20];
-    bool deleted;
+    
 };
 
-// void addPerson(struct person );
-// void readFromFile();
-// void editPassword(int, char , char, char );
-
-void addPerson(struct person input)
-{
+void addPerson(struct person person){
 
     FILE *outfile;
 
@@ -42,23 +36,51 @@ void addPerson(struct person input)
     }
 
     // write struct to file
-    fwrite(&input, sizeof(struct person), 1, outfile);
+    
+    fwrite(&person, sizeof(struct person), 1, outfile);
 
-    if (fwrite != 0)
-        printf("contents to file written successfully !\n");
-    else
+    if (fwrite == 0)
         printf("error writing file !\n");
+    
 
     fclose(outfile);
 }
 
-void readFromFile()
+/*
+void addPeopleToFile(struct person input[])
+{
+
+    FILE *outfile;
+
+    // open file for writing
+    outfile = fopen("person.dat", "a");
+    if (outfile == NULL)
+    {
+        fprintf(stderr, "\nError opened file\n");
+        exit(1);
+    }
+
+    printf(sizeof(input));
+
+    // write struct to file
+    for (int i = 0; i < sizeof(input); i++)
+    {
+        fwrite(&input[i], sizeof(struct person), 1, outfile);
+
+        if (fwrite == 0)
+            printf("error writing file !\n");
+    }
+
+    fclose(outfile);
+}*/
+
+struct person* readFromFile()
 {
 
     FILE *infile;
     struct person input;
+    struct person people[100];
 
-    // Open person.dat for reading
     infile = fopen("person.dat", "r");
     if (infile == NULL)
     {
@@ -66,50 +88,39 @@ void readFromFile()
         exit(1);
     }
 
-    // read file contents till end of file
+    int count = 0;
     while (fread(&input, sizeof(struct person), 1, infile))
-        printf("id = %d name = %s %s %s %s\n", input.id,
-               input.name, input.categories[1], input.passwords[0].title, input.passwords[1].title);
-
-    // close file
-    fclose(infile);
-}
-
-void editPassword(int id, char* title, char* field, char* new_value)
-{
-}
-
-void deletePassword(int id, char* title)
-{
-   FILE *infile;
-    struct person input;
-
-    // Open person.dat for reading
-    infile = fopen("person.dat", "ra");
-    if (infile == NULL)
     {
-        fprintf(stderr, "\nError opening file\n");
-        exit(1);
+        //printf("id = %d name = %s %s %s %s\n", input.id,
+              // input.name, input.categories[1], input.passwords[0].title, input.passwords[1].title);
+        people[count] = input;
+        count++;
     }
 
-    // read file contents till end of file
-    while (fread(&input, sizeof(struct person), 1, infile))
-        printf("id = %d name = %s %s %s %s\n", input.id,
-               input.name, input.categories[1], input.passwords[0].title, input.passwords[1].title);
+    fclose(infile);
+    struct person *returningPeople = people;
 
-    // close file
-    fclose(infile);   
+    return returningPeople;
 }
 
-void viewPassword(int id, char* title)
+void editPassword(int id, char *title, char *field, char *new_value)
 {
 }
 
-void addCategory(int id, char* new_category)
+void deletePassword(int id, char *title)
+{
+  
+}
+
+void viewPassword(int id, char *title)
 {
 }
 
-void viewCategory(int id, char* new_category)
+void addCategory(int id, char *new_category)
+{
+}
+
+void viewCategory(int id, char *new_category)
 {
 }
 
@@ -121,25 +132,24 @@ void changeName(int id)
 {
 }
 
-void findPerson(char* name)
+void findPerson(char *name)
 {
 }
-
-
 
 int main()
 {
     struct person input1 = {1, "alin", "69dsdfs", {"games", "school"}, {{"games", "csgo", "alintdg", "parola", "", "e veche"}, {"school", "docs", "alintutz", "parola22", "", "ok"}}};
-    struct person input2 = {3, "elena", "6sdsafs", {"games", "acasa"}, {{"games", "lol", "nebula", "alorap", "haide.com", ""}, {"games", "dbdl", "balenciaga", "parlica", "", "brnr"}}};
-
+    struct person input2 = {2, "elena", "6sdsafs", {"games", "acasa"}, {{"games", "lol", "nebula", "alorap", "haide.com", ""}, {"games", "dbdl", "balenciaga", "parlica", "", "brnr"}}};
+   
     //addPerson(input1);
     //addPerson(input2);
+  
 
-    //
+    // deletePassword(2, "lol");
 
-    deletePassword(2, "lol");
+    struct person *people = readFromFile();
 
-    //readFromFile();
+    printf("%s %s %s %s\n", people[0].name, people[1].categories[0],people[1].categories[1], people[0].passwords[1].passwrd);
 
     return 0;
 }
